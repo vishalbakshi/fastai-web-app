4ea32926cba69a4de5d4ae3932be1e8cb3fb1166
Vishal Bakshi <vdbakshi@gmail.com>
2020-08-10 13:25:26 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -28,7 +28,7 @@ async def setup_learner():
     data_bunch = ImageDataBunch.single_from_classes(path, classes,
         ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
     learn = create_cnn(data_bunch, models.resnet34, pretrained=False)
-    learn.load(model_file_name)
+    learn = load_learner(path/'models',f'{model_file_name}.pkl')
     return learn
 
 loop = asyncio.get_event_loop()
